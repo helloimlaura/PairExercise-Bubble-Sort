db33cf2f808d808f5ea33a73f4ab21418b3088ae
John Riccardi <riccjohn@gmail.com>
2018-07-30 19:41:03 -0500

    Updated test specs to use .toEqual instead of .toBe, and added test specs from review video

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,26 +1,55 @@
-describe('Split Array function', function() {
-  it('is able to split an array into two halves', function() {
-    expect(split([2, 2, 2, 3, 3, 3])).toBe([[2, 2, 2], [3, 3, 3]]);
-  });
-});
+describe('Merge sort', function() {
+  describe('split', function() {
+    if (
+      ('given one array, returns two arrays',
+      function() {
+        expected(split([])).toEqual([[], []]);
+      })
+    )
+      it('splits an array of even length', function() {
+        expect(split([5, 10])).toEqual([[5], [10]]);
+      });
 
-describe('Merge function', function() {
-  it('is able to merge two sorted arrays into one sorted array', function() {
-    const result = [1, 2, 3, 4, 5, 6];
-    expect(merge([1, 3, 4], [2, 5, 6])).toBe(result);
+    it('splits an array of odd length', function() {
+      expect(split([4, 10, 20])).toEqual([[4], [10, 20]]);
+    });
+    // end of split
   });
-});
 
-describe('mergeSort function', function() {
-  it('is able to sort an array with an even number of values', function() {
-    const result = [1, 2, 3, 5, 10, 11];
-    expect(mergeSort([1, 3, 5, 2, 10, 11])).toBe(result);
-  });
-});
+  describe('merge', function() {
+    it('given two arrays, returns an array', function() {
+      expect(merge([], [])).toEqual([]);
+    });
 
-describe('mergeSort function', function() {
-  it('is able to sort an array with an odd number of values', function() {
-    const result = [1, 2, 3, 5, 11];
-    expect(mergeSort([11, 3, 5, 2, 1])).toBe(result);
+    it('given two already sorted arrays of equal length, returns sorted result array', function() {
+      expect(merge([1, 5, 10], [2, 4, 11])).toEqual([1, 2, 4, 5, 10, 11]);
+      expect(merge([1, 9, 10], [2, 3, 8])).toEqual([1, 2, 3, 8, 9, 10]);
+    });
+
+    it('works for arrays of unequal length', function() {
+      expect(merge([1, 5, 10, 20, 21], [2, 4, 100])).toEqual([
+        1,
+        2,
+        4,
+        5,
+        10,
+        20,
+        21,
+        100,
+      ]);
+    });
+    // end of merge
   });
+  describe('mergeSort', function() {
+    it('with 1 or fewer elements, returns sorted array', function() {
+      expect(mergeSort([])).toEqual([]);
+      expect(mergeSort([1000])).toEqual([1000]);
+    });
+
+    it('does the thing its supposed to do', function() {
+      var sorted = mergeSort([9, 1994, 18, 1, -90, 1234, 56]);
+      expect(sorted).toEqual([-90, 1, 9, 18, 56, 1234, 1994]);
+    });
+  });
+  // end of mergeSort
 });
